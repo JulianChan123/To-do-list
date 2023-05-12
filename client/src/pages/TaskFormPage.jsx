00680c23b7e0203cc5e0df636e9433a1c043deb6
Julian Chan Palomo <73038870+JulianChan123@users.noreply.github.com>
2023-05-12 16:39:48 -0600

    crearTarea

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,9 +1,35 @@
+import {useForm} from 'react-hook-form'
+import { createTask } from '../api/tasks.api';
+import {useNavigate} from 'react-router-dom'
+
 export function TasksFormPage() {
+
+    const {register, handleSubmit, formState: {
+        errors
+    }} = useForm();
+
+    const navigate = useNavigate();
+
+    const onSubmit = handleSubmit(async data => {
+        await createTask(data);
+        navigate('/tasks')
+    })
+
     return(
         <div>
-            <form action="">
-                <input type="text" placeholder="Title"/>
-                <textarea rows="3" placeholder="Description"></textarea>
+            <form onSubmit={onSubmit}>
+                <input 
+                type="text" 
+                placeholder="Title"
+                {...register("title", {required : true})} 
+                />
+                {errors.title && <span>Este campo es requerido</span>}
+                <textarea 
+                rows="3" 
+                placeholder="Description"
+                {...register("description", {required : true})} 
+                ></textarea>
+                {errors.descriptionxc && <span>Este campo es requerido</span>}
                 <button>Save</button>
             </form>
         </div>
